5442d4d39325785e89eb82d7d90e5e4fe31435b1
GIHUNS-ASUS-TUF\gihun park <5p33dd3mon18@gmail.com>
2024-08-21 12:16:41 +0900

    로그인 기능 구현

--- src/main/java/himedia/project/bodamae/controller/user/HomeController.java
+++ src/main/java/himedia/project/bodamae/controller/user/HomeController.java
@@ -1,16 +1,16 @@
 package himedia.project.bodamae.controller.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import himedia.project.bodamae.repository.UserRepository;
 
 @Controller
 public class HomeController {
 
-	@GetMapping("/login")
-	public String login() {
-		
-		return "login";
-	}
+	
 	
 	
 }
