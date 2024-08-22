a5035143a5046b326e5ffc192bba1e54bb7b3697
GIHUNS-ASUS-TUF\gihun park <5p33dd3mon18@gmail.com>
2024-08-22 18:22:28 +0900

    로그인 페이지 분리

--- src/main/java/himedia/project/bodamae/controller/user/HomeController.java
+++ src/main/java/himedia/project/bodamae/controller/user/HomeController.java
@@ -1,16 +1,7 @@
 package himedia.project.bodamae.controller.user;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import himedia.project.bodamae.repository.UserRepository;
 
 @Controller
 public class HomeController {
-
-	
-	
-	
 }
